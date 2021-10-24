ec175567842cb1c13f2ac365e557fe3b8e03b4f5
Chee Kit <cheekit.chong98@gmail.com>
2021-10-24 20:13:53 +0800

    Add chevron animation

--- smart_shef_app/src/screens/settings/BluetoothScreen.tsx
+++ smart_shef_app/src/screens/settings/BluetoothScreen.tsx
@@ -1,5 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
+  Animated,
+  Easing,
   LayoutAnimation,
   Platform,
   ScrollView,
@@ -9,6 +11,7 @@ import {
   View,
 } from "react-native";
 import { Device } from "react-native-ble-plx";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import CTAButton from "../../components/elements/CTAButton";
@@ -35,6 +38,21 @@ const BluetoothScreen = () => {
   const [visible, setVisible] = useState(false);
   const [connectedDevice, setConnectedDevice] = useState<Device>();
   const [deviceExpanded, setDeviceExpanded] = useState(false);
+  const deviceSpinValue = useRef(new Animated.Value(0)).current;
+
+  const deviceRotate = deviceSpinValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", "180deg"],
+  });
+
+  const deviceSpin = () => {
+    Animated.timing(deviceSpinValue, {
+      toValue: deviceExpanded ? 0 : 1,
+      duration: 200,
+      easing: Easing.linear,
+      useNativeDriver: true,
+    }).start();
+  };
 
   const dispatch = useAppDispatch();
 
@@ -43,6 +61,7 @@ const BluetoothScreen = () => {
   const handlePressDeviceInfo = () => {
     LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
     setDeviceExpanded(!deviceExpanded);
+    deviceSpin();
   };
 
   const showModal = () => setVisible(true);
@@ -61,8 +80,6 @@ const BluetoothScreen = () => {
     const getDevice = async () => {
       const device = await getConnectedDevice(connectedDeviceUUID);
       if (device) {
-        console.log(device);
-
         setConnectedDevice(device);
       } else {
         dispatch(removeSelectedDeviceUUID());
@@ -84,6 +101,9 @@ const BluetoothScreen = () => {
             <TouchableNativeFeedback onPress={handlePressDeviceInfo}>
               <View style={styles.detailsTitle}>
                 <Title>Device Info</Title>
+                <Animated.View style={{ transform: [{ rotate: deviceRotate }] }}>
+                  <Icon name="chevron-down" size={30} />
+                </Animated.View>
               </View>
             </TouchableNativeFeedback>
             {deviceExpanded && (
@@ -120,7 +140,10 @@ const styles = StyleSheet.create({
     padding: SPACING.spacing_16,
   },
   detailsTitle: {
+    flexDirection: "row",
     padding: SPACING.spacing_16,
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   detailsContainer: {
     padding: SPACING.spacing_16,
